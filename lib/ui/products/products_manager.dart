92fdbe682e4bf69fcb7c5cb72699d697e1da0e52
Matuydod <eratordo2001@gmail.com>
2022-09-11 23:32:03 +0700

    Xay dung trang tong quan cac san pham

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -3,29 +3,39 @@ import '../../models/product.dart';
 class ProductsManager {
   final List<Product> _items = [
     Product(
-        title: "Red T-shirt",
-        description: "A T-shirt have color red",
-        price: 30.0,
-        imageUrl:
-            "https://images.asos-media.com/products/asos-design-oversized-t-shirt-in-red/12966810-1-red?wid=513&fit=constrain"),
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+      isFavorite: true,
+    ),
     Product(
-        title: "Black T-shirt",
-        description: "A T-shirt have color black",
-        price: 50.0,
-        imageUrl:
-            "https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmxhY2slMjB0JTIwc2hpcnR8ZW58MHx8MHx8&w=1000&q=80"),
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
     Product(
-        title: "Trouser Pants",
-        description: "A trouser pants light fabric striped beige",
-        price: 120.0,
-        imageUrl:
-            "https://martinvalen.com/17601-large_default/men-s-trousers-pants-light-fabric-striped-beige.jpg"),
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl:
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    ),
     Product(
-        title: "A Pan",
-        description: "A Pan",
-        price: 10.0,
-        imageUrl:
-            "https://www.thoughtco.com/thmb/GgqHVPv2O1uZq81iyCQC2xK2hKw=/3504x2336/filters:fill(auto,1)/skillet-frying-pan-on-white-background-131919734-57e41d555f9b586c35918f3f.jpg"),
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+      isFavorite: true,
+    ),
   ];
   int get itemCount {
     return _items.length;
