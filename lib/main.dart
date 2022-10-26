03b95b181ac626cb15f7d3ed5cd605501af899bb
Matuydod <eratordo2001@gmail.com>
2022-10-26 07:10:30 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'ui/products/products_manager.dart';
 // import 'ui/products/product_detail_screen.dart';
 // import 'ui/products/product_overview_screen.dart';
@@ -8,7 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'ui/screens.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -17,49 +19,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => ProductsManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => CartManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => OrdersManager(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'My Shop',
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.purple,
-          ).copyWith(
-            secondary: Colors.deepOrange,
+        providers: [
+          ChangeNotifierProvider(
+            create: (ctx) => AuthManager(),
           ),
-        ),
-        home: const ProductsOverviewScreen(),
-        routes: {
-          CartScreen.routeName: (ctx) => const OrdersScreen(),
-          OrdersScreen.routeName: (ctx) => const OrdersScreen(),
-          UserProductScreen.routeName: (ctx) => const UserProductScreen(),
-        },
-        onGenerateRoute: (settings) {
-          if (settings.name == EditProductScreen.routeName) {
-            final productId = settings.arguments as String?;
-            return MaterialPageRoute(
-              builder: (ctx) {
-                return EditProductScreen(
-                  productId != null
-                      ? ctx.read<ProductsManager>().findById(productId)
-                      : null,
-                );
+          ChangeNotifierProvider(
+            create: (ctx) => ProductsManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => CartManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => OrdersManager(),
+          ),
+        ],
+        child: Consumer<AuthManager>(
+          builder: (ctx, authManager, child) {
+            return MaterialApp(
+              title: 'My Shop',
+              theme: ThemeData(
+                fontFamily: 'Lato',
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.purple,
+                ).copyWith(
+                  secondary: Colors.deepOrange,
+                ),
+              ),
+              home: authManager.isAuth
+                  ? const ProductsOverviewScreen()
+                  : FutureBuilder(
+                      future: authManager.tryAutoLogin(),
+                      builder: (ctx, snapshot) {
+                        return snapshot.connectionState ==
+                                ConnectionState.waiting
+                            ? const SplashScreen()
+                            : const AuthScreen();
+                      }),
+              routes: {
+                CartScreen.routeName: (ctx) => const OrdersScreen(),
+                OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+                UserProductScreen.routeName: (ctx) => const UserProductScreen(),
+              },
+              onGenerateRoute: (settings) {
+                if (settings.name == EditProductScreen.routeName) {
+                  final productId = settings.arguments as String?;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return EditProductScreen(
+                        productId != null
+                            ? ctx.read<ProductsManager>().findById(productId)
+                            : null,
+                      );
+                    },
+                  );
+                }
+                return null;
               },
             );
-          }
-          return null;
-        },
-      ),
-    );
+          },
+        ));
   }
 }
